a57d3b1616d348f92c775c803a7a31e604c77865
Nishant Bhandari <79033833+nishant-out@users.noreply.github.com>
2022-01-14 13:55:27 +0530

    Update Main.java

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -4,24 +4,35 @@ public class Main{
 
 		LL list = new LL() ;
 
-		list.insert_Beg( 40 ) ;
-		list.insert_Beg( 30 ) ;
 		list.insert_Beg( 10 ) ;
+		list.insert_Beg( 30 ) ;
+		list.insert_Beg( 70 ) ;
 
 		list.insert_End( 50 ) ;
-		list.display() ;
+// 		list.display() ;
 		
 		list.insert_Rec( 20, 0 );
 		list.insert_Rec( 60, 5 );
-
         
-		list.display() ;
+        
+        LL lst2 = new LL();
+        lst2.insert_End( 5 );
+        lst2.insert_End( 4 );
+        lst2.insert_End( 3 );
+        list.insert_End( 2 );
+        
+        System.out.print( "Ques: " );
+		list.display();
+
+        list.bubbleSort();
+        
+        System.out.print( "Ans: " );
+        list.display();
 
 		// System.out.println( "MAin" );
 	}
 
 }
-
 
 
 
